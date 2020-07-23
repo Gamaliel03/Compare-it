13ae070bb7fff270702c043fca395680cee96d82
Gamaliel03 <armando.olivo@cbis.edu.sv>
2020-07-23 16:16:57 -0600

    Jueves 23 Proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FarmaciasComponent } from './farmacias/farmacias.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { MasBuscadosComponent } from './mas-buscados/mas-buscados.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+   path: '', component: InicioComponent 
+  },
+  {
+    path: 'masBuscados', component: MasBuscadosComponent
+  },
+  {
+    path: 'farmacias', component: FarmaciasComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InicioComponent } from './inicio/inicio.component';
+import { EncabezadoComponent } from './encabezado/encabezado.component';
+import { HomeComponent } from './home/home.component';
+import { MasBuscadosComponent } from './mas-buscados/mas-buscados.component';
+import { FarmaciasComponent } from './farmacias/farmacias.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent
+    InicioComponent,
+    EncabezadoComponent,
+    HomeComponent,
+    MasBuscadosComponent,
+    FarmaciasComponent
   ],
   imports: [
     BrowserModule,
