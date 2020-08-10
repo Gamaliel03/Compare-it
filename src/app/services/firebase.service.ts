801a427770d6ee8a714887adcc7a290fff5e02b3
Gamaliel03 <armando.olivo@cbis.edu.sv>
2020-08-10 16:04:46 -0600

    Consuomo de Firebase por servicio

--- src/app/services/firebase.service.ts
+++ src/app/services/firebase.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { from } from 'rxjs';
 import { AngularFirestore} from '@angular/fire/firestore';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -8,4 +9,23 @@ export class FirebaseService {
 
   constructor(private fireStore: AngularFirestore) { }
   
+  obtenerFarmacias(){
+    return this.fireStore.collection('farmacias').snapshotChanges();
+  }
+
+  obtenerMedicamentosPorFarmacia(farmaciaId: string){
+    return this.fireStore.collection('farmacias/'+farmaciaId+'/Medicamentos').snapshotChanges();
+  
+  }
+
+  obtenerFarmacia(farmaciaId: string){
+    return this.fireStore.doc('farmacias/'+farmaciaId).snapshotChanges();
+
+  }
+
+  obtenerUnMedicamentoPorFarmacia(farmaciaId: string, medicamentoId: string){
+    return this.fireStore.doc('farmacias/'+farmaciaId+'/Medicamentos/'+medicamentoId).snapshotChanges();
+  }
 }
+
+
